ffeb89044258bc47e09d3e563404b0f75967cda8
Francisco Aguilar <faguilar@undanet.com>
2023-01-12 00:24:36 +0100

    Config RabbitMQ, Project Mailer and Install Messenger in API

---
+++ api/src/Messenger/Message/UserRegistedMessage.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Messenger\Message;
+
+class UserRegistedMessage
+{
+    private string $id;
+    private string $name;
+    private string $email;
+    private string $token;
+
+    public function __construct(string $id, string $name, string $email, string $token)
+    {
+
+        $this->id = $id;
+        $this->name = $name;
+        $this->email = $email;
+        $this->token = $token;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getToken(): string
+    {
+        return $this->token;
+    }
+
+    /**
+     * @param string $token
+     */
+    public function setToken(string $token): void
+    {
+        $this->token = $token;
+    }
+
+
+
+}
